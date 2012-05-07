509352c4d06f37c5b224a022d063f920d7e58546
diabolo512 <lescot.charles@gmail.com>
2012-05-08 01:25:38 +0200

    add tests

--- jguard-core/src/test/java/net/sf/jguard/core/authentication/callbackhandler/JGuardCallbackHandlerTest.java
+++ jguard-core/src/test/java/net/sf/jguard/core/authentication/callbackhandler/JGuardCallbackHandlerTest.java
@@ -2,6 +2,7 @@ package net.sf.jguard.core.authentication.callbackhandler;
 
 import com.google.common.collect.Lists;
 import net.sf.jguard.core.authentication.callbacks.AuthenticationChallengeForCallbackHandlerException;
+import net.sf.jguard.core.authentication.callbacks.AuthenticationContinueForCallbackHandlerException;
 import net.sf.jguard.core.authentication.callbacks.AuthenticationSchemeHandlerCallback;
 import net.sf.jguard.core.authentication.schemes.AuthenticationSchemeHandler;
 import net.sf.jguard.core.authentication.schemes.DummyAuthenticationSchemeHandler;
@@ -120,8 +121,92 @@ public class JGuardCallbackHandlerTest {
         verify(authenticationSchemeHandler).authenticationFailed(request, response);
     }
 
+
+    @Test(expected = AuthenticationContinueForCallbackHandlerException.class)
+    public void test_handle_answer_to_challenge_true_and_challenge_needed_true_implies_continue() throws Exception {
+        //given
+        MockRequestAdapter request = new MockRequestAdapter(new MockRequest());
+        MockResponseAdapter response = new MockResponseAdapter(new MockResponse());
+
+        AuthenticationSchemeHandler<MockRequestAdapter, MockResponseAdapter> authenticationSchemeHandler = mock(AuthenticationSchemeHandler.class);
+        when(authenticationSchemeHandler.answerToChallenge(request, response)).thenReturn(true);
+        when(authenticationSchemeHandler.challengeNeeded(request, response)).thenReturn(true);
+        Collection<Class<? extends Callback>> callbackClasses = Lists.newArrayList();
+        callbackClasses.add(NameCallback.class);
+        when(authenticationSchemeHandler.getCallbackTypes()).thenReturn(callbackClasses);
+
+        Collection<AuthenticationSchemeHandler<MockRequestAdapter, MockResponseAdapter>> authenticationSchemeHandlers = new ArrayList<AuthenticationSchemeHandler<MockRequestAdapter, MockResponseAdapter>>();
+        authenticationSchemeHandlers.add(authenticationSchemeHandler);
+
+        CallbackHandler mockCallbackHandler = new AsynchronousMockCallbackHandler(request, response, authenticationSchemeHandlers);
+        List<Callback> callbackList = new ArrayList<Callback>();
+        NameCallback nameCallback = new NameCallback(DUMMY_PROMPT);
+        callbackList.add(nameCallback);
+        Callback[] callbacks = callbackList.toArray(new Callback[callbackList.size()]);
+
+        //when
+        mockCallbackHandler.handle(callbacks);
+        //then
+        verify(authenticationSchemeHandler).buildChallenge(request, response);
+    }
+
     @Test
-    public void test_handle_answer_to_challenge_false_and_authentication_needed_implies_build_challenge() throws Exception {
+    public void test_handle_answer_to_challenge_true_and_challenge_needed_false_implies_success() throws Exception {
+        //given
+        MockRequestAdapter request = new MockRequestAdapter(new MockRequest());
+        MockResponseAdapter response = new MockResponseAdapter(new MockResponse());
+
+        AuthenticationSchemeHandler<MockRequestAdapter, MockResponseAdapter> authenticationSchemeHandler = mock(AuthenticationSchemeHandler.class);
+        when(authenticationSchemeHandler.answerToChallenge(request, response)).thenReturn(true);
+        when(authenticationSchemeHandler.challengeNeeded(request, response)).thenReturn(false);
+        Collection<Class<? extends Callback>> callbackClasses = Lists.newArrayList();
+        callbackClasses.add(NameCallback.class);
+        when(authenticationSchemeHandler.getCallbackTypes()).thenReturn(callbackClasses);
+
+        Collection<AuthenticationSchemeHandler<MockRequestAdapter, MockResponseAdapter>> authenticationSchemeHandlers = new ArrayList<AuthenticationSchemeHandler<MockRequestAdapter, MockResponseAdapter>>();
+        authenticationSchemeHandlers.add(authenticationSchemeHandler);
+
+        CallbackHandler mockCallbackHandler = new AsynchronousMockCallbackHandler(request, response, authenticationSchemeHandlers);
+        List<Callback> callbackList = new ArrayList<Callback>();
+        NameCallback nameCallback = new NameCallback(DUMMY_PROMPT);
+        callbackList.add(nameCallback);
+        Callback[] callbacks = callbackList.toArray(new Callback[callbackList.size()]);
+
+        //when
+        mockCallbackHandler.handle(callbacks);
+
+    }
+
+    @Test
+    public void test_handle_answer_to_challenge_false_and_challenge_needed_false_implies_success() throws Exception {
+        //given
+        MockRequestAdapter request = new MockRequestAdapter(new MockRequest());
+        MockResponseAdapter response = new MockResponseAdapter(new MockResponse());
+
+        AuthenticationSchemeHandler<MockRequestAdapter, MockResponseAdapter> authenticationSchemeHandler = mock(AuthenticationSchemeHandler.class);
+        when(authenticationSchemeHandler.answerToChallenge(request, response)).thenReturn(false);
+        when(authenticationSchemeHandler.challengeNeeded(request, response)).thenReturn(false);
+        Collection<Class<? extends Callback>> callbackClasses = Lists.newArrayList();
+        callbackClasses.add(NameCallback.class);
+        when(authenticationSchemeHandler.getCallbackTypes()).thenReturn(callbackClasses);
+
+        Collection<AuthenticationSchemeHandler<MockRequestAdapter, MockResponseAdapter>> authenticationSchemeHandlers = new ArrayList<AuthenticationSchemeHandler<MockRequestAdapter, MockResponseAdapter>>();
+        authenticationSchemeHandlers.add(authenticationSchemeHandler);
+
+        CallbackHandler mockCallbackHandler = new AsynchronousMockCallbackHandler(request, response, authenticationSchemeHandlers);
+        List<Callback> callbackList = new ArrayList<Callback>();
+        NameCallback nameCallback = new NameCallback(DUMMY_PROMPT);
+        callbackList.add(nameCallback);
+        Callback[] callbacks = callbackList.toArray(new Callback[callbackList.size()]);
+
+        //when
+        mockCallbackHandler.handle(callbacks);
+
+    }
+
+
+    @Test(expected = AuthenticationChallengeForCallbackHandlerException.class)
+    public void test_handle_answer_to_challenge_false_and_challenge_needed_true_implies_failure() throws Exception {
         //given
         MockRequestAdapter request = new MockRequestAdapter(new MockRequest());
         MockResponseAdapter response = new MockResponseAdapter(new MockResponse());
@@ -146,34 +231,6 @@ public class JGuardCallbackHandlerTest {
         mockCallbackHandler.handle(callbacks);
         //then
         verify(authenticationSchemeHandler).buildChallenge(request, response);
-    }
-
-    @Test
-    public void test_handle_answer_to_challenge_false_and_authentication_not_needed_does_not_imply_build_challenge() throws Exception {
-        //given
-        MockRequestAdapter request = new MockRequestAdapter(new MockRequest());
-        MockResponseAdapter response = new MockResponseAdapter(new MockResponse());
-
-        AuthenticationSchemeHandler<MockRequestAdapter, MockResponseAdapter> authenticationSchemeHandler = mock(AuthenticationSchemeHandler.class);
-        when(authenticationSchemeHandler.answerToChallenge(request, response)).thenReturn(false);
-        when(authenticationSchemeHandler.challengeNeeded(request, response)).thenReturn(false);
-        Collection<Class<? extends Callback>> callbackClasses = Lists.newArrayList();
-        callbackClasses.add(NameCallback.class);
-        when(authenticationSchemeHandler.getCallbackTypes()).thenReturn(callbackClasses);
-
-        Collection<AuthenticationSchemeHandler<MockRequestAdapter, MockResponseAdapter>> authenticationSchemeHandlers = new ArrayList<AuthenticationSchemeHandler<MockRequestAdapter, MockResponseAdapter>>();
-        authenticationSchemeHandlers.add(authenticationSchemeHandler);
-
-        MockCallbackHandler mockCallbackHandler = new MockCallbackHandler(request, response, authenticationSchemeHandlers);
-        List<Callback> callbackList = new ArrayList<Callback>();
-        NameCallback nameCallback = new NameCallback(DUMMY_PROMPT);
-        callbackList.add(nameCallback);
-        Callback[] callbacks = callbackList.toArray(new Callback[callbackList.size()]);
-
-        //when
-        mockCallbackHandler.handle(callbacks);
-        //then
-        verify(authenticationSchemeHandler, never()).buildChallenge(request, response);
     }
 
 
