8ff13150ca8679e9b0791f5ec97d7eb9eef755c3
diabolo512 <lescot.charles@gmail.com>
2012-05-24 01:18:16 +0200

    enhance unit test

--- jguard-core/src/test/java/net/sf/jguard/core/authentication/loginmodules/UserLoginModuleTest.java
+++ jguard-core/src/test/java/net/sf/jguard/core/authentication/loginmodules/UserLoginModuleTest.java
@@ -10,32 +10,57 @@ import net.sf.jguard.core.lifecycle.MockRequestAdapter;
 import net.sf.jguard.core.lifecycle.MockResponseAdapter;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
 import javax.security.auth.Subject;
 import javax.security.auth.callback.Callback;
+import javax.security.auth.callback.CallbackHandler;
 import javax.security.auth.callback.NameCallback;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.verify;
+
 public class UserLoginModuleTest {
 
     private UserLoginModule userLoginModule;
     private ArrayList<AuthenticationSchemeHandler<MockRequestAdapter, MockResponseAdapter>> authenticationSchemeHandlers;
+    private MockCallbackHandler callbackHandler;
+    private MockAuthenticationManager mockAuthenticationManager;
+
+    @Mock
+    private CallbackHandler mockCallbackHandler;
+    @InjectMocks
+    private UserLoginModule mockUserLoginModule = new UserLoginModule() {
+        @Override
+        protected List<Callback> getCallbacks() {
+            List<Callback> list = Lists.newArrayList();
+            list.add(new NameCallback("dummy"));
+            return list;
+        }
+    };
 
     @Before
     public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+
         userLoginModule = new UserLoginModule() {
             @Override
             protected List<Callback> getCallbacks() {
                 List<Callback> list = Lists.newArrayList();
-                list.add(new NameCallback(""));
+                list.add(new NameCallback("dummy"));
                 return list;
             }
         };
         authenticationSchemeHandlers = new ArrayList<AuthenticationSchemeHandler<MockRequestAdapter, MockResponseAdapter>>();
         authenticationSchemeHandlers.add(new HookImplFormSchemeHandler(null));
+        callbackHandler = new MockCallbackHandler(null, null, authenticationSchemeHandlers);
+        mockAuthenticationManager = new MockAuthenticationManager("dummy application Name");
     }
 
     @Test(expected = IllegalArgumentException.class)
@@ -43,7 +68,7 @@ public class UserLoginModuleTest {
         Map<String, ?> shareState = new HashMap<String, Object>();
         Map<String, ?> options = new HashMap<String, Object>();
         options.put(JGuardAuthenticationManagerMarkups.AUTHENTICATION_MANAGER.getLabel(), null);
-        userLoginModule.initialize(new Subject(), new MockCallbackHandler(null, null, authenticationSchemeHandlers), shareState, options);
+        userLoginModule.initialize(new Subject(), callbackHandler, shareState, options);
     }
 
     @Test(expected = IllegalArgumentException.class)
@@ -51,17 +76,33 @@ public class UserLoginModuleTest {
         Map<String, ?> shareState = new HashMap<String, Object>();
         Map<String, ?> options = null;
 
-        userLoginModule.initialize(new Subject(), new MockCallbackHandler(null, null, authenticationSchemeHandlers), shareState, options);
+        userLoginModule.initialize(new Subject(), callbackHandler, shareState, options);
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void testInitialize_when_callbackhandler_is_null() throws Exception {
         Map<String, ?> shareState = new HashMap<String, Object>();
         Map<String, Object> options = new HashMap<String, Object>();
-        options.put(JGuardAuthenticationManagerMarkups.AUTHENTICATION_MANAGER.getLabel(), new MockAuthenticationManager("dummy application Name"));
+
+        options.put(JGuardAuthenticationManagerMarkups.AUTHENTICATION_MANAGER.getLabel(), mockAuthenticationManager);
         userLoginModule.initialize(new Subject(), null, shareState, options);
     }
 
+
+    @Test
+    public void testLogin() throws Exception {
+        initProperlyUserLoginmodule();
+        mockUserLoginModule.login();
+        verify(mockCallbackHandler).handle(any(Callback[].class));
+    }
+
+    private void initProperlyUserLoginmodule() {
+        Map<String, ?> shareState = new HashMap<String, Object>();
+        Map<String, Object> options = new HashMap<String, Object>();
+        options.put(JGuardAuthenticationManagerMarkups.AUTHENTICATION_MANAGER.getLabel(), mockAuthenticationManager);
+
+        mockUserLoginModule.initialize(new Subject(), mockCallbackHandler, shareState, options);
+    }
 
     @Test
     public void testLogout() throws Exception {
@@ -73,10 +114,6 @@ public class UserLoginModuleTest {
 
     }
 
-    @Test
-    public void testLogin() throws Exception {
-
-    }
 
     @Test
     public void testCommit() throws Exception {
